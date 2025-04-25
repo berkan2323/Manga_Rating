0fab8ff54b5091389a1c0292e5a0c23deba5bb97
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-25 08:22:04 +0300

    Add files via upload

---
+++ MangaRating/MangaRating/MangaRatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaRatingApp.swift
+//  MangaRating
+//
+//  Created by Berkan Aydın on 25.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MangaRatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
